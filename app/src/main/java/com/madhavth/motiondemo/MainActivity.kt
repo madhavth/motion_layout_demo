bb5cbd0c8d6aa7e4a4e8e4f1a338b72ca7480b39
madhavth <powermt2012@gmail.com>
2020-04-10 07:52:26 +0545

    init

---
+++ app/src/main/java/com/madhavth/motiondemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.madhavth.motiondemo
+
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import android.graphics.Rect
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.TransformationMethod
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
